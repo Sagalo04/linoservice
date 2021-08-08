ed2af1c5c2f1314b4b82a904f5a7e28fde0e35f5
Santiago García <49987163+Sagalo04@users.noreply.github.com>
2021-08-08 16:55:03 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,68 @@
+const app = require('express')();
+const http = require('http');
+const servidor = http.createServer(app);
+
+const SocketIO = require('socket.io')
+const io = SocketIO(servidor,{
+    cors: {
+        origin: '*'
+    }
+});
+
+//Doctor room
+const DOCTOR_ROOM = 'DOCTOR_ROOM';
+//requests
+var requests = [];
+
+function deleteRequest(id){
+    requests = requests.filter(request => request.id !== id);
+}
+
+io.on('connection', socket =>{
+    socket.on('request', (info)=>{
+        requests.push(info);
+        console.log('requests',requests)
+        //emit to all doctors
+        io.to(DOCTOR_ROOM).emit('requestDoctor', requests);
+    });
+
+    //are you a doctor?
+    socket.on('doctorSubscription', ()=>{
+        socket.join(DOCTOR_ROOM)
+    })
+
+    socket.on('retrievePrevRequests', (id)=>{
+        io.to(id).emit('requestDoctor', requests);
+    })
+
+    socket.on('response', (id, docInfo)=>{
+        deleteRequest(id);
+        io.to(id).emit('response', docInfo);
+    })
+
+    socket.on('delete', ()=>{
+        deleteRequest(socket.id);
+        io.to(DOCTOR_ROOM).emit('requestDoctor', requests);
+    })
+
+    socket.on('homeStart', id =>{
+        io.to(id).emit('homeStart');
+    })
+
+    socket.on('remoteStart', id =>{
+        io.to(id).emit('remoteStart');
+    })
+
+    socket.on('terminate', id =>{
+        io.to(id).emit('terminate');
+    })
+
+    //mensajes
+    socket.on('message', message =>{
+        io.to(message.to).emit('message', message);
+    })
+})
+
+servidor.listen(4000 || process.env.PORT, _=>{
+    console.log("Servidor inicializado");
+})
